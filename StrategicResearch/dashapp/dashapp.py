fc6af35206da42002ad6158bb64f82fc1f1ee4b7
nromano7 <nick.p.romano@gmail.com>
2018-12-01 10:43:50 -0500

    refactoring dashapp

--- StrategicResearch/dashapp/dashapp.py
+++ StrategicResearch/dashapp/dashapp.py
@@ -5,6 +5,7 @@ import dash_html_components as html#import Div, H1, H2, H3, H4, H5, H6, P, A, Sp
 import plotly.graph_objs as go
 import dashapp.figure as fig
 import dashapp.aggregate as aggregate
+from flask import url_for
 from flaskapp import application as flaskapp
 import textwrap
 
@@ -32,147 +33,156 @@ app = dash.Dash(__name__,
 	external_stylesheets=external_stylesheets
 )
 
-app.layout = html.Div(
-	className="dash-grid-container grey lighten-3",
-	children=[
-		html.Div(
-			id="filter-panel",
-			className="card card-body z-depth-1 pt-2",
-			children=[
-				html.H4("Filter Options"),
-				html.Div(
-					className="row",
-					children=[
-						html.Div(
-							className="col text-left",
-							children=[
-								html.H6("Filter by LTBP research topics."),
-								dcc.Dropdown(
-									id="tags-selection",
-									className="text-left",
-									options=[
-										dict(label="All Research Topics ", value="all"),
-										dict(label="Construction Quality", value="construction_quality"),
-										dict(label="Design & Details", value="design_and_details"),
-										dict(label="Material Specifications", value="material_specifications"),
-										dict(label="Live Load", value="live_load"),
-										dict(label="Environment", value="environment"),
-										dict(label="Maintenance & Preservation", value="maintenance_and_preservation"),
-										dict(label="Structural Integrity", value="structural_integrity"),
-										dict(label="Structural Condition", value="structural_condition"),
-										dict(label="Functionality", value="functionality"),
-										dict(label="Cost", value="cost")
-									],
-									placeholder="Select LTBP research topic...",
-									value="all",
-									searchable=False,
-									clearable=False
-								)
-							]
-						),
-						html.Div(
-							className="col text-left",
-							children=[
-								html.H6("Filter by bridge element."),
-								dcc.Dropdown(
-									id="record-set-selection",
-									className="text-left",
-									options=[
-										dict(label="All Bridges & Structures", value="all"),
-										dict(label="Bridges: All Elements", value="bridges"),
-										dict(label="Untreated Decks", value="untreated_deck"),
-										dict(label="Treated Decks", value="treated_deck"),
-										dict(label="Joints", value="joints"),
-										dict(label="Bearings", value="bearings"),
-										dict(label="Coatings", value="coatings")
-									],
-									placeholder="Select bridge element...",
-									value="all",
-									searchable=False,
-									clearable=False
-								)
-							]
-						),
-					],
-					style={"margin-bottom":"10px"}
-				),
-				html.Div(
-					className = "row",
-					style={"margin-top":"10px"},
-					children = [
-						html.Div(
-							className="col",
-							children=[
-								html.H6("Filter by Year"),
-								html.Div(
-									style={"margin-left":"20px","margin-right":"20px"},
-									children=[
-										dcc.RangeSlider(
-											marks={y: f'{y}' for y in range(2006, 2020)},
-											min=2006,
-											max=2019,
-											value=[2014,2019],
-											disabled=True
-										)
-									]
-								)
-							]
-						)
-					]
-				)
-			]
-		),
-		html.Div(
-			id="projects-panel",
-			className="card card-body text-center justify-content-center z-depth-1 p-1",
+app.layout = html.Div([
+	dcc.Location(id='url', refresh=False),
+	html.Div(
+		className="dash-grid-container grey lighten-3",
+		children=[
+			html.Div(
+				id="filter-panel",
+				className="card card-body z-depth-1 pt-2",
 				children=[
-				html.H2("{:,d}".format(aggregate.project_count()['total'])),
-				html.H3("Projects", className="text-muted")
-			]
-		),
-		html.Div(
-			id="publications-panel",
-			className="card card-body text-center justify-content-center z-depth-1 p-1",
-			children=[
-			html.H2("{:,d}".format(aggregate.publication_count())),
-			html.H3("Publications", className="text-muted")
-			]
-		),
-		dcc.Graph(
-			id='project-count-map',
-			className="z-depth-1",
-			figure = fig.project_count_map(),
-			config={'displayModeBar': False}
-		),
-		dcc.Graph(
-			id='funding-heatmap',
-			className="z-depth-1",
-			figure=fig.funding_heatmap(),
-			config={'displayModeBar': False}
-		),
-		dcc.Graph(
-			id='barchart1',
-			className="z-depth-1",
-			config={'displayModeBar': False}
-		),
-		dcc.Graph(
-			id='barchart2',
-			className="z-depth-1",
-			config={'displayModeBar': False}
-		),
-		dcc.Graph(
-			id='barchart3',
-			className="z-depth-1",
-			config={'displayModeBar': False}
-		),
-		html.H4("Attributes",id="barchart1-heading",className="p-2 text-left h-25"),
-		html.H4("Inputs",id="barchart2-heading",className="p-2 text-left h-25"),
-		html.H4("Performance",id="barchart3-heading",className="p-2 text-left h-25"),
-		html.H4("Count by State",id="project-count-map-heading",className="p-2 text-left"),
-		html.H4("Funding by State",id="funding-heatmap-heading",className="p-2 text-left")
-	]
-)
-
+					html.H4("Filter Options"),
+					html.Div(
+						className="row",
+						children=[
+							html.Div(
+								className="col text-left",
+								children=[
+									html.H6("Filter by LTBP research topics."),
+									dcc.Dropdown(
+										id="tags-selection",
+										className="text-left",
+										options=[
+											dict(label="All Research Topics ", value="all"),
+											dict(label="Construction Quality", value="construction_quality"),
+											dict(label="Design & Details", value="design_and_details"),
+											dict(label="Material Specifications", value="material_specifications"),
+											dict(label="Live Load", value="live_load"),
+											dict(label="Environment", value="environment"),
+											dict(label="Maintenance & Preservation", value="maintenance_and_preservation"),
+											dict(label="Structural Integrity", value="structural_integrity"),
+											dict(label="Structural Condition", value="structural_condition"),
+											dict(label="Functionality", value="functionality"),
+											dict(label="Cost", value="cost")
+										],
+										placeholder="Select LTBP research topic...",
+										value="all",
+										searchable=False,
+										clearable=False
+									)
+								]
+							),
+							html.Div(
+								className="col text-left",
+								children=[
+									html.H6("Filter by bridge element."),
+									dcc.Dropdown(
+										id="record-set-selection",
+										className="text-left",
+										options=[
+											dict(label="All Bridges & Structures", value="all"),
+											dict(label="All Superstrcuture", value="superstructure"),
+											dict(label="Untreated Decks", value="untreated_deck"),
+											dict(label="Treated Decks", value="treated_deck"),
+											dict(label="Joints", value="joints"),
+											dict(label="Bearings", value="bearings"),
+											dict(label="Steel Coatings", value="coatings"),
+											dict(label="Prestressing", value="prestressing")
+										],
+										placeholder="Select bridge element...",
+										value="all",
+										searchable=False,
+										clearable=False
+									)
+								]
+							),
+						],
+						style={"margin-bottom":"10px"}
+					),
+					html.Div(
+						className = "row",
+						style={"margin-top":"10px"},
+						children = [
+							html.Div(
+								className="col",
+								children=[
+									html.H6("Filter by Year"),
+									html.Div(
+										style={"margin-left":"20px","margin-right":"20px"},
+										children=[
+											dcc.RangeSlider(
+												marks={y: f'{y}' for y in range(2006, 2020)},
+												min=2006,
+												max=2019,
+												value=[2014,2019],
+												disabled=True
+											)
+										]
+									)
+								]
+							)
+						]
+					)
+				]
+			),
+			html.Div(
+				id="projects-panel",
+				className="card card-body text-center justify-content-center z-depth-1 p-1",
+					children=[
+					html.H2("{:,d}".format(aggregate.project_count()['total'])),
+					html.H3("Projects", className="text-muted")
+				]
+			),
+			html.Div(
+				id="publications-panel",
+				className="card card-body text-center justify-content-center z-depth-1 p-1",
+				children=[
+				html.H2("{:,d}".format(aggregate.publication_count())),
+				html.H3("Publications", className="text-muted")
+				]
+			),
+			dcc.Graph(
+				id='project-count-map',
+				className="z-depth-1",
+				figure = fig.project_count_map(),
+				config={'displayModeBar': False}
+			),
+			dcc.Graph(
+				id='funding-heatmap',
+				className="z-depth-1",
+				# figure=fig.funding_heatmap(),
+				config={'displayModeBar': False}
+			),
+			dcc.Graph(
+				id='barchart1',
+				className="z-depth-1",
+				config={'displayModeBar': False}
+			),
+			dcc.Graph(
+				id='barchart2',
+				className="z-depth-1",
+				config={'displayModeBar': False}
+			),
+			dcc.Graph(
+				id='barchart3',
+				className="z-depth-1",
+				config={'displayModeBar': False}
+			),
+			html.H4("Attributes",id="barchart1-heading",className="p-2 text-left h-25"),
+			html.H4("Inputs",id="barchart2-heading",className="p-2 text-left h-25"),
+			html.H4("Performance",id="barchart3-heading",className="p-2 text-left h-25"),
+			html.H4("Count by State",id="project-count-map-heading",className="p-2 text-left"),
+			html.H4("Funding by State",id="funding-heatmap-heading",className="p-2 text-left"),
+			# html.Button(children=[dcc.Link('Redirect', href='/explore')], id='barchart1-projects-button',className="p-1 text-center h-20 btn btn-light btn"),
+			# html.A('redirect', 
+				# id='barchart1-publications-button',
+				# className="p-1 text-center h-20 btn btn-light btn",
+			# 	href="http://127.0.0.1:5000/", 
+			# 	target="_blank")
+		]
+	)
+])
 
 
 
@@ -181,14 +191,68 @@ app.layout = html.Div(
 
 """ ------------------------ CALLBACKS --------------------------- """
 
+""" calback function for redirecting on click """
+# @app.callback(dash.dependencies.Output('page-content', 'children'),
+#               [dash.dependencies.Input('url', 'pathname')])
+# def display_page(pathname):
+# 	print(pathname)
+# 	return {}
 
 
+
+""" callback function for redirecting user on performance barchart click """
+@app.callback(
+        dash.dependencies.Output('barchart3-heading', 'children'),
+        [dash.dependencies.Input('barchart3', 'clickData')])
+def on_attr_click(selection):
+	if selection is None:
+		return "Test"
+	else:
+		print(len(selection['points'][0]['customdata']))
+		return "Test"
+
+
+""" callback function for redirecting user on input barchart click """
+@app.callback(
+        dash.dependencies.Output('barchart2-heading', 'children'),
+        [dash.dependencies.Input('barchart2', 'clickData')])
+def on_attr_click(selection):
+	if selection is None:
+		return "Test"
+	else:
+		print(len(selection['points'][0]['customdata']))
+		return "Test"
+
+
+""" callback function for redirecting user on attr barchart click """
+@app.callback(
+        dash.dependencies.Output('barchart1-heading', 'children'),
+        [dash.dependencies.Input('barchart1', 'clickData')])
+def on_attr_click(selection):
+	if selection is None:
+		return "Test"
+	else:
+		print(len(selection['points'][0]['customdata']))
+		return "Test"
+
+
+""" callback function for redirecting user on map click """
+@app.callback(
+        dash.dependencies.Output('project-count-map-heading', 'children'),
+        [dash.dependencies.Input('project-count-map', 'clickData')])
+def on_map_click(selection):
+	if selection is None:
+		return "Test"
+	else:
+		print(len(selection['points'][0]['customdata']))
+		return selection['points'][0]['text']
+
+
+""" callback function for updating the project count """
 @app.callback(dash.dependencies.Output('projects-panel','children'),
 	[dash.dependencies.Input('tags-selection','value'),
 		dash.dependencies.Input('record-set-selection','value')])
-
 def callback_project_count(tag, element_tag):
-	""" callback function for updating the project count """
 	queries = dict(tag=tag, element_tag=element_tag)
 	children=[
 		html.H2("{:,d}".format(aggregate.project_count(queries = queries)['total'])),
@@ -197,13 +261,11 @@ def callback_project_count(tag, element_tag):
 	return children
 
 
-
+""" callback function for updating the publication count """
 @app.callback(dash.dependencies.Output('publications-panel','children'),
 	[dash.dependencies.Input('tags-selection','value'),
 		dash.dependencies.Input('record-set-selection','value')])
-
 def callback_publication_count(tag, element_tag):
-	""" callback function for updating the publication count """
 	queries = dict(tag=tag, element_tag=element_tag)
 	children=[
 		html.H2("{:,d}".format(aggregate.publication_count(queries=queries))),
@@ -212,90 +274,89 @@ def callback_publication_count(tag, element_tag):
 	return children
 
 
-
+""" callback function for updating the project count map """
 @app.callback(dash.dependencies.Output('project-count-map','figure'),
 	[dash.dependencies.Input('tags-selection','value'),
 		dash.dependencies.Input('record-set-selection','value')])
-
 def callback_project_count_map(tag, element_tag):
-	""" callback function for updating the project count map """
 	queries = dict(tag=tag, element_tag=element_tag)
 	data = aggregate.project_count_by_state(queries=queries)
 	figure = fig.project_count_map(data=data)
 	return figure
 
 
-
+""" callback function for updating attribute bar chart """
 @app.callback(dash.dependencies.Output('barchart1','figure'),
 	[dash.dependencies.Input('tags-selection','value'),
 		dash.dependencies.Input('record-set-selection','value')])
-
 def callback_attribute_barchart(topic_selection, element_selection):
-	""" callback function for updating attribute bar chart """
 	# format bar labels
 	attributes=['construction_quality','design_and_details','material_specifications']
 	formatted_attributes=[attr.title().replace("_"," ").replace("And","&") for attr in attributes]
 	labels=["<br>".join(textwrap.wrap(label, width=15)) for label in formatted_attributes]
-	# get project counts
-	counts=[aggregate.project_count_by_topic(topic=attr, 
+	# get project counts and doc_ids
+	counts, ids = [], []
+	for attr in attributes:
+		count, id = aggregate.project_count_by_topic(topic=attr, 
 			element=element_selection, 
-			topic_selection=topic_selection) 
-			for attr in attributes]
+			topic_selection=topic_selection)
+		counts.append(count)
+		ids.append(id)
 	# generate figure
-	figure = fig.bar_chart(labels=labels, counts=counts)
+	figure = fig.bar_chart(labels=labels, counts=counts, ids=ids)
 	return figure
 
 
-
+""" callback function for updating inputs bar chart """
 @app.callback(dash.dependencies.Output('barchart2','figure'),
 	[dash.dependencies.Input('tags-selection','value'),
 		dash.dependencies.Input('record-set-selection','value')])
-
 def callback_input_barchart(topic_selection, element_selection):
-	""" callback function for updating inputs bar chart """
 	# format bar labels
 	inputs=['live_load', 'environment', 'maintenance_and_preservation']
 	formatted_inputs=[inp.title().replace("_"," ").replace("And","&") for inp in inputs]
 	labels=["<br>".join(textwrap.wrap(label, width=15)) for label in formatted_inputs]
-	# get project counts
-	counts=[aggregate.project_count_by_topic(topic=inp, 
+	# get project counts and doc_ids
+	counts, ids = [], []
+	for i in inputs:
+		count, id = aggregate.project_count_by_topic(topic=i, 
 			element=element_selection, 
-			topic_selection=topic_selection) 
-			for inp in inputs]
+			topic_selection=topic_selection)
+		counts.append(count)
+		ids.append(id)
 	# generate figure
-	figure = fig.bar_chart(labels=labels, counts=counts)
+	figure = fig.bar_chart(labels=labels, counts=counts, ids=ids)
 	return figure
 
 
-
+""" callback function for updating performance bar chart """
 @app.callback(dash.dependencies.Output('barchart3','figure'),
 	[dash.dependencies.Input('tags-selection','value'),
 		dash.dependencies.Input('record-set-selection','value')])
-
 def callback_performance_barchart(topic_selection, element_selection):
-	""" callback function for updating performance bar chart """
 	# format bar labels
 	performance=['structural_integrity', 'structural_condition', 'functionality', 'cost']
 	formatted_performance=[perf.title().replace("_"," ").replace("And","&") for perf in performance]
 	labels=["<br>".join(textwrap.wrap(label, width=15)) for label in formatted_performance]
-	# get project counts
-	counts=[aggregate.project_count_by_topic(topic=perf, 
+	# get project counts and doc_ids
+	counts, ids = [], []
+	for p in performance:
+		count, id = aggregate.project_count_by_topic(topic=p, 
 			element=element_selection, 
-			topic_selection=topic_selection) 
-			for perf in performance]
+			topic_selection=topic_selection)
+		counts.append(count)
+		ids.append(id)
 	# generate figure
-	figure = fig.bar_chart(labels=labels, counts=counts)
+	figure = fig.bar_chart(labels=labels, counts=counts, ids=ids)
 	return figure
 
 
-
+""" callback function for updating funding heat map """
 @app.callback(dash.dependencies.Output('funding-heatmap','figure'),
 	[dash.dependencies.Input('tags-selection','value'),
 		dash.dependencies.Input('record-set-selection','value')])
-
 def callback_funding_heamap(topic_selection, element_selection):
-	""" callback function for updating funding heat map """
-	data =aggregate.funding_by_state(topic=topic_selection, element=element_selection)
+	data = aggregate.funding_by_state(topic=topic_selection, element=element_selection)
 	figure = fig.funding_heatmap(data=data)
 	return figure
 
--- StrategicResearch/dashapp/figure.py
+++ StrategicResearch/dashapp/figure.py
@@ -7,180 +7,213 @@ with open("./dashapp/app/static/statesGeo.json") as f:
 	geo = json.load(f)
 
 # states geo JSON
-def getStatesGeo():
-	with open('./statesGeo.json','r') as f:
-		geo = json.load(f)
-	return geo
+# def getStatesGeo():
+# 	with open('./statesGeo.json','r') as f:
+# 		geo = json.load(f)
+# 	return geo
+
 
 def project_count_map(data=None):
 
-	mapbox_access_token = r"pk.eyJ1IjoibnJvbWFubzciLCJhIjoiY2ppa2prYjQ2MWszczNsbnh5YnhkZTh1aSJ9.5qBV5E8g3oxlo3ZFL4n6Zw"
-	state_shapefile_PATH = r"./dashapp/app/static/geojson"
+	MAPBOX_ACCESS_TOKEN = r"pk.eyJ1IjoibnJvbWFubzciLCJhIjoiY2ppa2prYjQ2MWszczNsbnh5YnhkZTh1aSJ9.5qBV5E8g3oxlo3ZFL4n6Zw"
+	STATES_SHAPEFILE_PATH = r"./dashapp/app/static/geojson"
+	BINS = {
+		range(0, 1): [], range(1, 5): [], range(5, 10): [],
+		range(10, 15): [], range(15, 20): [], range(20, 25): [],
+		range(25, 30): [], range(30, 35): [], range(35, 40): [],
+		range(40, 45): [], range(45, 50): [], range(50, 200): []
+	}
+	COLORSCALE = [
+		"#ffffff", "#eeeeee", "#e0e0e0",
+		"#bdbdbd", "#deebf7", "#c6dbef",
+		"#9ecae1", "#6baed6", "#4292c6",
+		"#2171b5", "#08519c", "#08306b"
+	]
 
+	# retrieve data if none provided
 	if data == None:
 		data = aggregate.project_count_by_state()
 
-	BINS = {
-		range(0,1):[],
-		range(1,5):[],
-		range(5,10):[],
-		range(10,15):[],
-		range(15,20):[],
-		range(20,25):[],
-		range(25,30):[],
-		range(30,35):[],
-		range(35,40):[],
-		range(40,45):[],
-		range(45,50):[],
-		range(50,200):[]
-	}
-
-	COLORSCALE = [
-		"#ffffff",
-		"#eeeeee",
-		"#e0e0e0",
-		"#bdbdbd",
-		"#deebf7",
-		"#c6dbef",
-		"#9ecae1",
-		"#6baed6",
-		"#4292c6",
-		"#2171b5",
-		"#08519c",
-		"#08306b"
-	]
+	# bins, bin counts, and bin colors
 	colormap = dict(zip(BINS, COLORSCALE))
-	[BINS[key].append(item[0]) for key in BINS for item in list(data.items()) if item[1] in key]
+	for item in list(data.items()):
+		for key in BINS:
+			if item[1].get('doc_count') in key:
+				BINS[key].append(item[0])
 
-	annotations = [dict(
-		showarrow = False,
-		align = 'left',
-		text = "",
-		# text = '<b>Count of Projects <br> Funded Per State</b>',
-		font=dict(family='Courier New, monospace', size=18),
-		x = 0.95,
-		y = 0.85,
-	)]
+	# get list of states with data
+	states_with_data = []
+	[states_with_data.append(s) for l in BINS.values() for s in l]
 
+	# legend set up
+	annotations = []
 	for i, bin in enumerate(BINS):
-		color = colormap[bin]
-		min_ = min(bin)
-		max_ = max(bin)
-		label = f"{min_} - {max_}"
-		if min_ == max_:
-			label = f"{min_}"
-
 		annotations.append(
 			dict(
-				arrowcolor = color,
-				align = 'left',
-				text = label,
-				x = 0.95,
-				y = 0.65-(i/20),
-				ax = -60,
-				ay = 0,
-				arrowwidth = 10,
-				arrowhead = 0,
-				font=dict(family='Courier New, monospace', size=14),
-				bgcolor = '#EFEFEE'
+				arrowcolor=colormap[bin],
+				arrowhead=0,
+				arrowwidth=10,
+				align='left',
+				ax=-60,
+				ay=0,
+				bgcolor='#FFFFFF',
+				font=dict(
+					family='Courier New, monospace', 
+					size=14
+				),
+				text=f"{min(bin)} - {max(bin)}" if min(bin) != max(bin) else f"{min(bin)}",
+				x=0.95,
+				y=0.75-(i/20)
 			)
 		)
 
-	
-	layout = dict(
-		mapbox = dict(
-			layers = [],
-			accesstoken = mapbox_access_token,
-			style='light',
+	# set up map layout
+	layout = dict(		
+		annotations=annotations,
+		hovermode="closest",
+		hoverlabel=dict(
+			bgcolor='#3E4551'
+		),
+		mapbox=dict(
+			accesstoken=MAPBOX_ACCESS_TOKEN,
 			center=dict(
-				lat=39,
+				lat=39, 
 				lon=-96
 			),
+			layers=[],
 			pitch=0,
+			style='light',
 			zoom=3.1,
 		),
-		hovermode="closest",
-		hoverlabel = dict(
-			bgcolor = '#3E4551'
-		),
-		margin = dict(
+		margin=dict(
 			l=0.01,
 			r=0.01,
 			t=0.01,
 			b=0.01
-		),
-		annotations=annotations
+		)
 	)
 
-	states_with_data = []
-	[states_with_data.append(s) for l in BINS.values() for s in l]
-	# print(states_with_data)
-		
-	text, lat, lon = [], [], []
-	for f1 in os.listdir(state_shapefile_PATH):
+	# set up layers for states 
+	text, lat, lon, customdata = [], [], [], []
+	for f1 in os.listdir(STATES_SHAPEFILE_PATH):
 
-		state = f1.split(".")[0]
-
-		lat.append(geo[state]['latitude'])
-		lon.append(geo[state]['longitude'])
-		hovertext = (f"{geo[state]['full']}" +
-			f"<br>Project Count: {data.get(state, 0)}")
-		text.append(hovertext)
-
-		with open(os.path.join(state_shapefile_PATH, f1),'r') as f2:
+		# get state shape file
+		with open(os.path.join(STATES_SHAPEFILE_PATH, f1), 'r') as f2:
 			state_shapefile = json.load(f2)
 
+		# for each state
+		state = f1.split(".")[0]
 
+		# append lat/lon data points
+		lat.append(geo[state]['latitude'])
+		lon.append(geo[state]['longitude'])
+
+		# append customdata
+		if data.get(state):
+			customdata.append(data.get(state).get('doc_ids'))
+		else:
+			customdata.append(None)
+
+		# append hovertext
+		if data.get(state):
+			count = data.get(state).get('doc_count')
+		else:
+			count = 0
+		hovertext = (f"{geo[state]['full']}" +
+					 f"<br>Total Projects: {count}")
+		text.append(hovertext)
+
+		# append geolayers to layout
 		if state in states_with_data:
 			for b in BINS:
 				if state in BINS[b]:
 					color = colormap[b]
 					geo_layer = dict(
-						sourcetype = 'geojson',
-						source = state_shapefile,
-						type = 'fill',
-						color = color,
-						opacity = 0.6
+						color=color,
+						opacity=0.6,
+						sourcetype='geojson',
+						source=state_shapefile,
+						type='fill'
 					)
 					layout['mapbox']['layers'].append(geo_layer)
 		else:
 			color = COLORSCALE[0]
 			geo_layer = dict(
-				sourcetype = 'geojson',
-				source = state_shapefile,
-				type = 'fill',
-				color = color,
-				opacity = 0.7
+				
+				color=color,
+				opacity=0.7,
+				sourcetype='geojson',
+				source=state_shapefile,
+				type='fill'
 			)
 			layout['mapbox']['layers'].append(geo_layer)
 
+	# create trace dict for layers
 	trace = dict(
+		customdata=customdata,
+		hoverinfo="text",
 		lat=lat,
 		lon=lon,
-		text=text,   
-		type = 'scattermapbox',
-		hoverinfo="text",
-		marker = dict(size=20, color='white', opacity=0)
+		marker=dict(size=20, color='white', opacity=0),
+		text=text,
+		type='scattermapbox'
 	)
+
 	figure = dict(data=[trace], layout=layout)
+	return figure
+
+def bar_chart(labels, counts, ids):
+
+	# set up trace
+	trace = go.Bar(
+		hoverinfo="none",
+		customdata=[id for id in ids],
+		insidetextfont=dict(
+			color="black",
+			size=14
+		),
+		marker=dict(color="#3F729B"),
+		outsidetextfont=dict(
+			color="black",
+			size=14
+		),
+		text=[count for count in counts],
+		textposition="auto",
+		x=labels,
+		y=counts
+	)
+
+	# set up layout
+	layout = go.Layout(
+		margin={'l': 70, 'r': 25, 't': 60, 'b': 40, },
+		xaxis=dict(showticklabels=True),
+		yaxis=dict(title="Record Counts")
+	)
+
+	figure = go.Figure(data=[trace], layout=layout)
 
 	return figure
+
+
+
 
 def funding_heatmap(data=None):
 
 	# retrieve data and remove 'DC'
 	if not data:
 		data = aggregate.funding_by_state(topic="all", element="all")
-	states = [state for state in data.keys() if (state != 'DC') and (sum([b["doc_count"] for b in list(data[state].values())]) != 0)]
+	states = [state for state in data.keys() if (state != 'DC') and (
+		sum([b["doc_count"] for b in list(data[state].values())]) != 0)]
 	# state_labels = [geo[state.upper()]['abbrv'] if len(state) == 2 else geo[state]['abbrv'] finally state for state in states]
 
-	#filter
+	# filter
 
 	# format data for heatmap
 	X = states
 	Y = ["$<100k", "$100-250k", "$250-500k", "$500-750k", "$750k-1M", "$1M+"]
 	Z = []
-	buckets = ['0.0-100000.0','100000.0-250000.0','250000.0-500000.0','500000.0-750000.0','750000.0-1000000.0','1000000.0-*']
+	buckets = ['0.0-100000.0', '100000.0-250000.0', '250000.0-500000.0',
+			   '500000.0-750000.0', '750000.0-1000000.0', '1000000.0-*']
 	for b in buckets:
 		z = []
 		for state in states:
@@ -192,7 +225,8 @@ def funding_heatmap(data=None):
 	for yi, yy in enumerate(Y):
 		hovertext.append([])
 		for xi, xx in enumerate(X):
-			hovertext[-1].append(f'State:  {xx}<br />Dollar Amount:  {yy}<br />Project Count:  {Z[yi][xi]}')
+			hovertext[-1].append(
+				f'State:  {xx}<br />Dollar Amount:  {yy}<br />Project Count:  {Z[yi][xi]}')
 
 	trace = go.Heatmap(
 		z=Z,
@@ -209,49 +243,20 @@ def funding_heatmap(data=None):
 			ypad=0
 		),
 		hoverinfo='text',
-		text=hovertext                
+		text=hovertext
 	)
 
-	
 	layout = go.Layout(
 		xaxis=dict(title="States"),
 		yaxis=dict(
 			title="Funding (Dollar Amount)",
 			tickprefix="  "
 		),
-		margin={'l':110,'r':10,'t':50,'b':70,},
+		margin={'l': 110, 'r': 10, 't': 50, 'b': 70, },
 	)
 
 	figure = go.Figure(data=[trace], layout=layout)
 
 	return figure
 
-def bar_chart(**kwargs):
 
-	trace = go.Bar(
-		x=kwargs.get('labels'),
-		y=kwargs.get('counts'),
-		text=[label for label in kwargs.get('labels')],
-		textposition="auto",
-		# opacity=0.5,
-		marker=dict(color="#3F729B"),
-		hoverinfo="text",
-		hovertext=kwargs.get('counts'),
-		insidetextfont=dict(
-			color="black",
-			size=14
-		),
-		outsidetextfont=dict(
-			color="black",
-			size=14
-		),
-	)
-
-	layout = go.Layout(
-		margin={'l':35,'r':25,'t':50,'b':15,},
-		xaxis=dict(showticklabels=False),
-	)
-
-	figure = go.Figure(data=[trace], layout=layout)
-
-	return figure
