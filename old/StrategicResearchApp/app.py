a4d9824a73f36ab3469ae07f0f5d1e46dbc731db
nromano7 <nick.p.romano@gmail.com>
2019-01-17 12:57:49 -0500

    clean up file structure

--- old/StrategicResearchApp/app.py
+++
@@ -1,4 +0,0 @@
-from StrategicResearch import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- old/StrategicResearchApp/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, url_for, flash, redirect
-from StrategicResearch import app
-
-@app.route("/")
-@app.route("/home")
-def home():
-  return render_template('home.html')
