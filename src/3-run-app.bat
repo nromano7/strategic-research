d52fae64a328c19ac4078ce77a179119e8336cc6
nromano7 <nick.p.romano@gmail.com>
2019-02-15 16:52:57 -0500

    update batch file

--- src/3-run-app.bat
+++ src/3-run-app.bat
@@ -1,4 +1,11 @@
 @echo on
+docker start elasticsearch
+IF /I "%ERRORLEVEL%" NEQ "0" ( 
+docker volume create --name esdata
+docker run --name elasticsearch -d -p 9200:9200 -p 9300:9300 ^
+-e "discovery.type=single-node" -v esdata:/usr/share/elasticsearch/data ^
+docker.elastic.co/elasticsearch/elasticsearch:6.3.2
+)
 docker pull nromano7/srs:latest
 docker run --name srs-app --rm -p 5000:5000 ^
 --link elasticsearch:elasticsearch ^
