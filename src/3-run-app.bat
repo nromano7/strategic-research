79c7ded6f6018a76b5dc64a3d9fd15d95d5ad68d
nromano7 <nick.p.romano@gmail.com>
2019-04-02 15:58:21 -0400

    update batch files

--- src/3-run-app.bat
+++ src/3-run-app.bat
@@ -10,5 +10,5 @@ docker pull nromano7/srs:latest
 docker run --name srs-app --rm -p 5000:5000 ^
 --link elasticsearch:elasticsearch ^
 --env ELASTICSEARCH_URL=http://elasticsearch:9200 ^
--d nromano7/srs:latest application.py
+-d nromano7/srs:latest run.py
 pause
